9e8e1e7bd25381865b61a6edf907fb70a1ffb894
Amit <amitkr24031995@gmail.com>
2021-09-27 12:53:55 +0530

    Adding Welcome Message

---
+++ WelcomeEmpWAge/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WelcomeEmpWAge
+{
+    class Program
+    {
+        //Non Static Method
+        public void welcomeEmployee()
+        {
+            Console.WriteLine("Welcome to Employee Wages");
+        }
+
+
+        //Static method
+        public static string EmpName()
+        {
+            string name = "Amit";
+            return name;
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.welcomeEmployee();
+            Console.WriteLine(EmpName());
+        }
+    }
+}
