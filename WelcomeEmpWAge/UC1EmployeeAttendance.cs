4d4d13ad986e4fe35a798952c00faa041d239a00
Amit <amitkr24031995@gmail.com>
2021-09-27 12:57:51 +0530

    Adding UC1EmployeeAttendance

---
+++ WelcomeEmpWAge/UC1EmployeeAttendance.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace UC1EmployeeAttendance
+{
+    class Employee
+    {
+        //constant
+        public int IS_FULL_TIME;
+
+
+        //Constructor 
+        Employee()
+        {
+            IS_FULL_TIME = 1;
+        }
+        static void Main(string[] args)
+        {
+            //Computation
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            Employee empwage = new Employee();
+            if (empCheck == empwage.IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee Is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
