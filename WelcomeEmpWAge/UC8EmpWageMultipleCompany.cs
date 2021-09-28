5e93d5e9f4d67dea7f9daadf1a567fda84408f62
Amit <amitkr24031995@gmail.com>
2021-09-28 17:51:06 +0530

    Adding UC8EmpWageMultipleCompany

---
+++ WelcomeEmpWAge/UC8EmpWageMultipleCompany.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace UC8EmpWageMultipleCompany
+{
+    class Program
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public static int computeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            //Variable
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            //Computation
+            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random rand = new Random();
+                int empCheck = rand.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Days : " + totalWorkingDays + "Emp Hours: " + empHrs);
+            }
+            int totalEmpWage = totalEmpHrs * empRatePerHour;
+            Console.WriteLine("Total Emp Wage For Company: " + company + " is " + totalEmpWage);
+            return totalEmpWage;
+        }
+        static void Main(string[] args)
+        {
+            computeEmpWage("DMart", 20, 2, 10);
+            computeEmpWage("Relience", 10, 4, 20);
+        }
+    }
+}
